24185f6cfbaf09b99996a69525cfa197e8f67b31
diniamo <diniamo53@gmail.com>
2024-11-21 22:52:59 +0100

    style: cleanup

--- lua/run/init.lua
+++ lua/run/init.lua
@@ -3,16 +3,12 @@ local M = {}
 local terminal = require("run.terminal")
 
 local function effective_cwd()
-    local git = vim.fs.find(".git", {upward = true, type = "directory"})
+    local git = vim.fs.find(".git", { upward = true, type = "directory" })
     return git[1] or vim.uv.cwd()
 end
 
 local function prompt_command()
     return vim.fn.input("Run command: ")
-end
-
-local function run_handler(cmd)
-    M.run(cmd.args, cmd.bang)
 end
 
 function M.run(command, override)
@@ -40,7 +36,7 @@ function M.setup(config)
     M.config = require("run.config")(config)
     M.cache = require("run.cache")
 
-    vim.api.nvim_create_user_command("Run", run_handler, {bang = true, nargs = "?"})
+    vim.api.nvim_create_user_command("Run", function(cmd) M.run(cmd.args, cmd.bang) end, { bang = true, nargs = "?" })
 end
 
 return M
